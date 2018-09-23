80ff8c6220e5783eb75c26d4c4c7e1a71ed6b0df
KhaNAbylAi <abylaikhan.zhumagazy@nu.edu.kz>
2018-09-23 21:12:26 +0600

    my first commit

--- src/main/java/kz/edu/nu/cs/exericse/MyService.java
+++ src/main/java/kz/edu/nu/cs/exericse/MyService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -20,13 +21,32 @@ public class MyService {
     @GET
     public Response getItems() {
         Gson gson = new Gson();
-        String json = gson.toJson(entries);
+        String json = gson.toJson(entries.keySet());
         return Response.ok(json).build();
+    }
+    @GET
+    @Path ("{ key : [a -zA -Z_0-9]+}")
+    public Response getByKey (@PathParam ("key") String key) {
+    String r = "Requested item with the key \"" + key +  "\" is " + entries.get(key);
+    return Response.ok(r).build();
     }
     
     @POST
     public Response createEntry(@FormParam("mykey") String mykey, @FormParam("myentry") String myentry) {
-        entries.put(mykey, myentry);
-        return Response.status(Status.CREATED).build();
+    	
+    	if(entries.containsKey(mykey)) {
+        	return Response.status(Status.FORBIDDEN).build();
+        }
+        else if(mykey.contains(" ") || myentry.contains("\n")){
+        	return Response.status(Status.BAD_REQUEST).build();
+        }
+        else if(mykey.equals("")) {
+        	return Response.status(Status.NOT_ACCEPTABLE).build();
+        }
+        else {
+	    	entries.put(mykey, myentry);
+	    	return Response.ok(entries.keySet()).build();
+	        //return Response.status(Status.CREATED).build();
+        }
     }
 }
